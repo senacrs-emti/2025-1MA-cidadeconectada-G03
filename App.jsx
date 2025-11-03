eb0e5764a116b62595a4ca032dbc4d7705bec0f1
danielciarlini <daniel.p.ciarlini@gmail.com>
2025-11-03 11:58:56 -0300

    teste

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Meu App</h1>
+      {/* Conteúdo do app */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
